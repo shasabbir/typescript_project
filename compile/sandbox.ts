3a0fae1525c55ca2d8014b4f2d0a01415e56d570
shasabbir <shasabbir234@gmail.com>
2023-04-12 02:31:02 +0600

    compilation done

--- compile/sandbox.ts
+++ compile/sandbox.ts
@@ -1,10 +1,12 @@
 var character = 'mario';
+
 console.log(character);
 var inputs = document.querySelectorAll('input');
 inputs.forEach(function (input) {
     console.log(input);
 });
-var circle = function (diameter) {
+
+var circle = (diameter:number) => {
     return diameter * Math.PI;
-};
+}
 console.log(circle(3));
